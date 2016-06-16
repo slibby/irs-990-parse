bc33da32752a2474e7e9dde3c1dd9074009a2fc2
Sam Libby <slibby@esri.com>
2016-06-16 15:57:14 -0600

    Update download_files.py

--- scripts/download_files.py
+++ scripts/download_files.py
@@ -2,8 +2,9 @@ import csv, boto
 conn = boto.connect_s3()
 irs990 = conn.get_bucket('irs-form-990')
 with open("vermont_filings.csv", "r") as csvfile:
+    csvfile.readline()
     datareader = csv.reader(csvfile)
     for row in datareader:
         filename = row[1][row[1].rfind("/")+1:]
-        k = irs990.get_key('201242289349303124_public.xml')
+        k = irs990.get_key(filename)
         k.get_contents_to_filename(r"data\\{}".format(filename))
