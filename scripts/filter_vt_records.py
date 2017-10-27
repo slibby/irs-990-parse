5591c2573280fcb0d27e0d1fa4267ddb8be50d92
Elijah Lofgren <elijahlofgren@gmail.com>
2017-10-26 19:56:00 -0500

    Add missing space

--- scripts/filter_vt_records.py
+++ scripts/filter_vt_records.py
@@ -9,7 +9,7 @@ with open(r"c:\temp\irs990\output.csv", "w", newline = '') as o:
             if prefix == 'AllFilings.item.URL' and len(value)>0:
                 obj = {'url':value}
             if prefix == 'AllFilings.item.OrganizationName':
-                if "VERMONT" in valueor "VT " in value or " VT " in value:
+                if "VERMONT" in value or "VT " in value or " VT " in value:
                     obj['name'] = value
                     #print(obj)
                     dataWriter.writerow([obj['name'].strip(),obj['url'].replace("\\n","")])
